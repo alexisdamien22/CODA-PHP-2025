8cf8d37333fe90a0c2d1403bb0638ce74741ca7a
Alexis DAMIEN <alexisdamien@icloud.com>
2025-11-20 16:53:16 +0100

    added j1/ex-5 (affichage)

---
+++ Exercices/coda-bph-j1/affichage/ex-5.php
@@ -0,0 +1,39 @@
+<?php
+    $grades = [
+        "french" => [12, 9, 13],
+        "english" => [18, 12, 11],
+        "maths" => [15, 11, 13]
+    ];
+    $i = 0;
+?>
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice 5</title>
+    </head>
+    <body>
+        <h1>
+            Liste des notes
+        </h1>
+        <ul>
+            <?php
+            foreach($grades as $name => $var)
+            {
+                echo"<li>$name : ";
+                while($i < count($var))
+                {
+                    echo"$var[$i]";
+                    if($i < count($var)-1)
+                    {
+                        echo", ";
+                    }
+                    $i++;
+                }
+                $i = 0;
+                echo"</li>";
+            }?>
+        </ul>
+    </body>
+</html>
