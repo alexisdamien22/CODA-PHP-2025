5b311d1d77edcba482584facf614caaead37fe4a
Alexis DAMIEN <alexisdamien@icloud.com>
2025-11-20 15:55:41 +0100

    fic=xes j1/ex-4 (conditions et boucles)

--- Exercices/coda-bph-j1/conditions-et-boucles/ex-4.php
+++ Exercices/coda-bph-j1/conditions-et-boucles/ex-4.php
@@ -10,12 +10,21 @@
         ]
     ];
     $i = 0;
-    while($i < count($users))
+    //premier essai (un peu capilotracté...)
+    /*while($i < count($users))
     {
         foreach($users[$i] as $key => $user)
         {
             echo "User $key : $user <br>";
         }
         $i++;
+    }*/
+    //deuxieme essai
+    while($i < count($users))
+    {
+        $firstName = $users[$i]["firstName"];
+        $lastName = $users[$i]["lastName"];
+        $i++;
+        echo "$firstName $lastName<br>";
     }
 ?>
