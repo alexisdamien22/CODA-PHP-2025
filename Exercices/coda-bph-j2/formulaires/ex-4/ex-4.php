c4985cfe101dbc72c4af723ae340c63289bc5fb4
Alexis DAMIEN <alexisdamien@icloud.com>
2025-11-21 11:23:59 +0100

    added j2/ex-4 (formulaires)

---
+++ Exercices/coda-bph-j2/formulaires/ex-4/ex-4.php
@@ -0,0 +1,52 @@
+<?php
+$users = [
+    [
+        "name" => "Mari",
+        "favoriteColor" => "blue"
+    ],
+    [
+        "name" => "Santa",
+        "favoriteColor" => "red"
+    ],
+    [
+        "name" => "Shrek",
+        "favoriteColor" => "green"
+    ],
+    [
+        "name" => "Iron Man",
+        "favoriteColor" => "red"
+    ],
+    [
+        "name" => "Hulk",
+        "favoriteColor" => "green"
+    ],
+    [
+        "name" => "Hugues",
+        "favoriteColor" => "blue"
+    ]
+];
+$i = 0;
+$color = $_GET["color"];
+?>
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice 4</title>
+    </head>
+    <body>
+        <ul>
+            <?php
+            while($i < count($users))
+            {
+                if($users[$i]["favoriteColor"] === $color)
+                {
+                    echo'<li>';
+                    echo $users[$i]["name"];
+                }
+                echo"</li>";
+                $i++;
+            }?>
+        </ul>
+    </body>
+</html>
