bec3a3f77620e05a8e59522953cdc3afae27faf1
Alexis DAMIEN <alexisdamien@icloud.com>
2025-11-24 14:44:56 +0100

    did step 1

---
+++ Exercices/coda-bph-j3/ex-bonus/index.php
@@ -0,0 +1,26 @@
+<?php
+function routing() : string
+{
+    if(isset($_GET["route"]))
+    {
+        if($_GET["route"]==="about")
+        {
+            return "about";
+        }
+        elseif($_GET["route"]==="contact")
+        {
+            return "contact";
+        }
+        else
+        {
+            return "homepage";
+        }
+    }
+    else
+    {            
+        return "homepage";
+    }
+}
+require "templates/layout.phtml";
+
+?>
