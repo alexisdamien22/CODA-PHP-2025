8e813881248063776bc6549a10d395b96fea3aae
Alexis DAMIEN <alexisdamien@icloud.com>
2025-12-01 11:02:04 +0100

    routing apache lesson

---
+++ Exercices/coda-bph-j7/routing-apache/controllers/BlogController.php
@@ -0,0 +1,13 @@
+<?php
+class BlogController
+{
+    public function index()
+    {
+        require "templates/blog_index.phtml";
+    }
+
+    public function show(int $id)
+    {
+        require "templates/blog_show.phtml";
+    }
+}
