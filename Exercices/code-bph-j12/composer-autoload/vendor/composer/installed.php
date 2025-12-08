0fa262dc5a49329a8498df5290d165e81cc0272a
Alexis DAMIEN <alexisdamien@icloud.com>
2025-12-08 10:32:11 +0100

    j12 added composer-autoload

---
+++ Exercices/code-bph-j12/composer-autoload/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alexis/composer-autoload',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b4b0fdc216019e66f3cf3265df64f5cc0eab6163',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alexis/composer-autoload' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b4b0fdc216019e66f3cf3265df64f5cc0eab6163',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
